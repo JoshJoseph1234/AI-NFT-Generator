194d4e961c792e279dbe8a885a0d09de809bde2f
JoshJoseph1234 <joshjosephcod@gmail.com>
2025-02-03 16:29:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
